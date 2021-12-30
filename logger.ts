6ca3d5acc142c8087ea2b7f781f4c4e3ed16a15b
GHOSCHT <31184695+GHOSCHT@users.noreply.github.com>
2021-12-30 19:01:39 +0100

    Change logging directory

--- logger.ts
+++ logger.ts
@@ -5,7 +5,7 @@ export const logger = winston.createLogger({
   format: winston.format.json(),
   transports: [
     new winston.transports.Console({ format: winston.format.simple() }),
-    new winston.transports.File({ filename: "error.log", level: "error" }),
-    new winston.transports.File({ filename: "combined.log" }),
+    new winston.transports.File({ filename: "logs/error.log", level: "error" }),
+    new winston.transports.File({ filename: "logs/combined.log" }),
   ],
 });
